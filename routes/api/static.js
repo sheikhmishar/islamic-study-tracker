f8b775e97e6937e96e5ae0c5945133ee558c981c
sheikhMishar00 <sheikhMishar00@sheikhMishar.com>
2020-07-23 14:55:00 +0600

    -{}

--- routes/api/static.js
+++ routes/api/static.js
@@ -3,11 +3,11 @@ const staticRouter = express.Router()
 const { static } = express
 const { join } = require('path')
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production')
   staticRouter.get('/', (req, res) =>
     res.status(200).json({ message: 'server alive' })
   )
-} else {
+else {
   const rootPath = join(__dirname, '..', '..', 'views', 'build')
   staticRouter.use('/', static(rootPath))
   const indexPath = join(rootPath, 'index.html')
